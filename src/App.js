a9b9a6c0401671fb6cb4f21f20bb4f72dae7dc1f
yettimon <globalobjectiveart@gmail.com>
2021-01-06 18:58:30 +0100

    package-lock json edit + moving shit to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './components/User/User';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -12,6 +15,7 @@ function App() {
         <p>Test 1st react-based SPA</p>
         <User></User>
     </div>
+    <Footer></Footer>
     </div>
   );
 }
